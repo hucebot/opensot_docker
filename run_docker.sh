3a6bed550e7dac4bb7236308e6b035b05d8d344f
EnricoMingo <enricomingo@gmail.com>
2024-04-23 17:17:10 +0200

    removing unnecessary from script

--- run_docker.sh
+++ run_docker.sh
@@ -7,11 +7,6 @@ docker run \
             --rm \
             --env DISPLAY=$DISPLAY \
             --privileged \
-            --gpus 'all,"capabilities=compute,utility,graphics"' \
             --volume /tmp/.X11-unix:/tmp/.X11-unix  \
-            --volume /dev:/dev \
             opensot
-            #--volume $(pwd):/home/code \
-            #--volume /home/enrico/catkin_ws/external/kangaroo_mujoco:/home/code/kangaroo_mujoco \
-            #--device=/dev/dri/card0  kangarl
-    
+            
