1f14b327ebb114040e2ac65ad169b1798754d714
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2024-10-16 14:44:44 +0200

    Remove personal changes

--- run_docker.sh
+++ run_docker.sh
@@ -8,6 +8,4 @@ docker run \
 	--env DISPLAY=$DISPLAY \
 	--privileged \
 	--volume /tmp/.X11-unix:/tmp/.X11-unix \
-	-v $(pwd)/../unitree_ros:/home/forest_ws/src/unitree_ros \
-	-v $(pwd)/../g1_opensot:/home/forest_ws/src/g1_opensot \
 	opensot
