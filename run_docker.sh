6b78bf3c31c78e29cf0caa2a3fe65a9f9aac4292
Ioannis Loizou <yiannisloizou@gmail.com>
2025-02-19 15:45:49 +0100

    Remove submodules

--- run_docker.sh
+++ run_docker.sh
@@ -14,8 +14,9 @@ if [ $isRunning -eq 0 ]; then
 		--env DISPLAY=$DISPLAY \
 		--privileged \
 		--volume /tmp/.X11-unix:/tmp/.X11-unix \
-		--volume $(pwd)/unitree_ros:/home/forest_ws/src/unitree_ros \
-    		--volume $(pwd)/g1_opensot:/home/forest_ws/src/g1_opensot \
+		--volume $(pwd)/../unitree_ros:/home/forest_ws/src/unitree_ros \
+    		--volume $(pwd)/../g1_opensot:/home/forest_ws/src/g1_opensot \
+    		--volume $(pwd)/../srbd_mpc:/home/forest_ws/src/srbd_mpc \
 		opensot
 
 else
