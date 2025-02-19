1968ae9de1a0233d2127f5ed2d605ff5403882ae
Ioannis Loizou <yiannisloizou@gmail.com>
2025-02-19 13:33:37 +0100

    Add g1 locomotion files

--- run_docker.sh
+++ run_docker.sh
@@ -14,6 +14,9 @@ if [ $isRunning -eq 0 ]; then
 		--env DISPLAY=$DISPLAY \
 		--privileged \
 		--volume /tmp/.X11-unix:/tmp/.X11-unix \
+		--volume $(pwd)/unitree_ros:/home/forest_ws/src/unitree_ros \
+    		--volume $(pwd)/g1_opensot:/home/forest_ws/src/g1_opensot \
+    		--volume $(pwd)/srbd-mpc:/home/forest_ws/src/srbd-mpc \
 		opensot
 
 else
