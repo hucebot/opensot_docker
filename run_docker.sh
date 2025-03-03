a9083f99f362e5d96221100a42a68db500661c5c
Ioannis Loizou <yiannisloizou@gmail.com>
2025-03-03 15:01:57 +0100

    Add g1_mujoco_sim package

--- run_docker.sh
+++ run_docker.sh
@@ -6,18 +6,18 @@ isRunning=`docker ps -f name=opensot | grep -c "opensot"`;
 if [ $isRunning -eq 0 ]; then
 	docker rm opensot
 	docker run \
+		--gpus all \
 		--name opensot  \
 		--interactive \
 		--tty \
 		--net host \
-		--rm \
 		--env DISPLAY=$DISPLAY \
 		--privileged \
 		--volume /tmp/.X11-unix:/tmp/.X11-unix \
 		--volume $(pwd)/../unitree_ros:/home/forest_ws/src/unitree_ros \
     		--volume $(pwd)/../g1_opensot:/home/forest_ws/src/g1_opensot \
     		--volume $(pwd)/../srbd_mpc:/home/forest_ws/src/srbd_mpc \
-		opensot
+    		opensot
 
 else
     echo "Docker already running."
