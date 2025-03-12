98899fe9639f9ebfa955a1626c16bae709cf6b36
Ioannis Loizou <yiannisloizou@gmail.com>
2025-03-12 10:37:12 +0100

    Add gpu support for lab laptop

--- run_docker.sh
+++ run_docker.sh
@@ -6,7 +6,8 @@ isRunning=`docker ps -f name=opensot | grep -c "opensot"`;
 if [ $isRunning -eq 0 ]; then
 	docker rm opensot
 	docker run \
-		--gpus all \
+		--gpus 'all,"capabilities=compute,utility,graphics"' \
+		--env NVIDIA_DRIVER_CAPABILITIES=all \
 		--name opensot  \
 		--interactive \
 		--tty \
@@ -14,10 +15,13 @@ if [ $isRunning -eq 0 ]; then
 		--env DISPLAY=$DISPLAY \
 		--privileged \
 		--volume /tmp/.X11-unix:/tmp/.X11-unix \
+		--device /dev/dri \
+		--volume /dev:/dev \
+		--volume /usr/share/glvnd/egl_vendor.d/10_nvidia.json:/usr/share/glvnd/egl_vendor.d/10_nvidia.json \
 		--volume $(pwd)/../unitree_ros:/home/forest_ws/src/unitree_ros \
-    		--volume $(pwd)/../g1_opensot:/home/forest_ws/src/g1_opensot \
-    		--volume $(pwd)/../srbd_mpc:/home/forest_ws/src/srbd_mpc \
-    		opensot
+    --volume $(pwd)/../g1_opensot:/home/forest_ws/src/g1_opensot \
+ 		--volume $(pwd)/../srbd_mpc:/home/forest_ws/src/srbd_mpc \
+		opensot
 
 else
     echo "Docker already running."
