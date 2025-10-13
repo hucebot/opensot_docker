03df1b08caf456350b4b05bc59d6324b40d8b579
ioloizou <yiannisloizou@gmail.com>
2025-10-13 10:28:01 +0200

    Refactor run_docker.sh to streamline Docker run command and remove unnecessary volume mounts

--- run_docker.sh
+++ run_docker.sh
@@ -4,24 +4,19 @@ xhost +
 isRunning=`docker ps -f name=opensot | grep -c "opensot"`;
 
 if [ $isRunning -eq 0 ]; then
-	#docker remove opensot
-	docker run \
+    #docker remove opensot
+    docker rm -f opensot
+    docker run \
 		--gpus 'all,"capabilities=compute,utility,graphics"' \
 		--env NVIDIA_DRIVER_CAPABILITIES=all \
-		--name opensot  \
-		--interactive \
-		--tty \
-		--net host \
-		--env DISPLAY=$DISPLAY \
-		--privileged \
-		--volume /tmp/.X11-unix:/tmp/.X11-unix \
-		--device /dev/dri \
-		--volume /dev:/dev \
-		--volume /usr/share/glvnd/egl_vendor.d/10_nvidia.json:/usr/share/glvnd/egl_vendor.d/10_nvidia.json \
-		--volume $(pwd)/../unitree_ros:/home/forest_ws/src/unitree_ros \
-    --volume $(pwd)/../g1_opensot:/home/forest_ws/src/g1_opensot \
- 		--volume $(pwd)/../srbd_mpc:/home/forest_ws/src/srbd_mpc \
-		opensot
+        --name opensot  \
+        --interactive \
+        --tty \
+        --net host \
+        --env DISPLAY=$DISPLAY \
+        --privileged \
+        --volume /tmp/.X11-unix:/tmp/.X11-unix \
+        opensot
 
 else
     echo "Docker already running."
